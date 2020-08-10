b1ffa71221b319639ec448b6ed03f52ace388536
garistvlad <vv.garist@gmail.com>
2020-08-10 05:39:30 +0300

    daily update 2020-08-10

--- flask_app/models.py
+++ flask_app/models.py
@@ -11,6 +11,7 @@ class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
+    is_admin = db.Column(db.Boolean)
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
